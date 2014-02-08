6485825aa75a9aaa6fd4ec8e9e41369cd0493f50
Paden <padenportillo@gmail.com>
2014-02-08 11:08:03 -0600

    a lot of stuff
    Signed-off-by: Paden <padenportillo@gmail.com>

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,3 @@
 'use strict';
 
-angular.module('eastApp', ['ngAnimate', '$strap']);
+angular.module('eastApp', ['ngAnimate', '$strap', 'google-maps']);
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,14 +1,17 @@
 'use strict';
 
-angular.module('eastApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-    $scope.popover = {
-	  "title": "Title",
-	  "content": "Hello Popover<br />This is a multiline message!"
-	};
-  });
+angular.module('eastApp').controller('MainCtrl', function ($scope)
+{
+    $scope.popover =
+    {
+        'title': 'Title',
+        'content': 'Hello Popover<br />This is a multiline message!'
+    };
+    $scope.map = {
+        zoom: 8,
+        center: {
+            latitude: 45,
+            longitude: -73
+        }
+    };
+});

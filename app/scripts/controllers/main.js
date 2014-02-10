368d4aefa7e5e38cbba389af312c9138484a04b4
Paden <padenportillo@gmail.com>
2014-02-10 13:08:03 -0600

    new bootstrap + map theme
    Signed-off-by: Paden <padenportillo@gmail.com>

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -14,4 +14,43 @@ angular.module('eastApp').controller('MainCtrl', function ($scope)
             longitude: -73
         }
     };
+
+    $scope.mapOptions = {
+        styles : [
+          {
+            featureType: 'road',
+            elementType: 'geometry',
+            stylers: [
+              { color: '#FF8BA4' },
+            ]
+          },{
+            featureType: 'administrative.neighborhood',
+            elementType: 'geometry',
+            stylers: [
+              { color: '#333' },
+              { weight: 1.6 }
+            ]
+          }, {
+            featureType: 'road',
+            elementType: 'labels',
+            stylers: [
+              { color: '#FF8BA4' },
+              { invert_lightness: true }
+            ]
+          }, {
+            featureType: 'landscape',
+            elementType: 'geometry',
+            stylers: [
+              { color: '#ccc' },
+            ]
+          }, {
+            featureType: 'poi.school',
+            elementType: 'geometry',
+            stylers: [
+              { hue: '#f90' },
+              { lightness: -15 },
+              { saturation: 99 }
+            ]
+          }
+        ]};
 });

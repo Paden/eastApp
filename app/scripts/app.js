c57c30be71e3710b1c9d622fb166d6a4599c10c0
Paden <padenportillo@gmail.com>
2014-02-12 20:51:35 -0600

    Created views and bower stuff
    Signed-off-by: Paden <padenportillo@gmail.com>

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,27 @@
 'use strict';
 
-angular.module('eastApp', ['ngAnimate', '$strap', 'google-maps']);
+angular.module('eastApp', ['ngRoute', 'ngAnimate', '$strap', 'google-maps']);
+
+angular.module('eastApp').config([ '$routeProvider', '$locationProvider', function($routeProvider, $locationProvider)
+{
+	var directory      = 'views/',
+		availableViews = [ 'home', 'artists', 'exhibitions', 'events', 'sponsors', 'tour' ],
+		defView        = availableViews[0];
+
+	$routeProvider.when('/:view?',
+	{
+        title       : 'East App',
+        templateUrl : function( $routeParams )
+        {
+			var view       =  $routeParams.view,
+				isGoodView =  availableViews.indexOf( view ) >= 0,
+				template   =  isGoodView ? view : defView;
+
+            return directory + template + '.html';
+        }
+    });
+
+    $routeProvider.otherwise( {redirectTo : '/' } );
+
+    $locationProvider.hashPrefix('!');
+}]);

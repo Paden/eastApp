b98ba37351616dbbabe08b73672e354176f74e3d
Paden <padenportillo@gmail.com>
2014-02-15 10:33:33 -0600

    map ctrl
    Signed-off-by: Paden <padenportillo@gmail.com>

---
+++ app/scripts/controllers/map.js
@@ -0,0 +1,82 @@
+'use strict';
+
+angular.module('eastApp').controller('MapCtrl', function ($scope)
+{
+    $scope.map = { zoom: 15, center: { latitude: 30.2646809, longitude: -97.6979548, } };
+
+    $scope.mapOptions = {
+        styles : [
+        {
+            'featureType': 'water',
+            'stylers': [
+                {
+                    'color': '#46bcec'
+                },
+                {
+                    'visibility': 'on'
+                }
+            ]
+        },
+        {
+            'featureType': 'landscape',
+            'stylers': [
+                {
+                    'color': '#f2f2f2'
+                }
+            ]
+        },
+        {
+            'featureType': 'road',
+            'stylers': [
+                {
+                    'saturation': -100
+                },
+                {
+                    'lightness': 45
+                }
+            ]
+        },
+        {
+            'featureType': 'road.highway',
+            'stylers': [
+                {
+                    'visibility': 'simplified'
+                }
+            ]
+        },
+        {
+            'featureType': 'road.arterial',
+            'elementType': 'labels.icon',
+            'stylers': [
+                {
+                    'visibility': 'off'
+                }
+            ]
+        },
+        {
+            'featureType': 'administrative',
+            'elementType': 'labels.text.fill',
+            'stylers': [
+                {
+                    'color': '#444444'
+                }
+            ]
+        },
+        {
+            'featureType': 'transit',
+            'stylers': [
+                {
+                    'visibility': 'off'
+                }
+            ]
+        },
+        {
+            'featureType': 'poi',
+            'stylers': [
+                {
+                    'visibility': 'off'
+                }
+            ]
+        }
+    ]};
+  });
--- app/scripts/services/artflask.js
+++ app/scripts/services/artflask.js
@@ -2,13 +2,23 @@
 
 angular.module('eastApp').service('Artflask', ['$resource', function Artflask ( $resource )
 {
-	var endPoint     = 'http://east.gluu.org',
-		artResource  =  $resource(endPoint + '/art/:art_id/:action', {art_id:'@id', action: 'picture' } );
+	var endPoint         = 'http://east.gluu.org',
+		artResource      =  $resource(endPoint + '/art/:art_id/:action', { art_id:'@id' } ),
+		artistsResource  =  $resource(endPoint + '/artists/:artist_id'),
+		eventsResource   =  $resource(endPoint + '/events/:event_name'),
+		venuesResource   =  $resource(endPoint + '/venues/:venue_id'),
+		registerResource =  $resource(endPoint + '/register/:registration_id', {registration_id : '@id' }),
+		profileResource  =  $resource(endPoint + '/profile'),
+		staffResource    =  $resource(endPoint + '/staff/:staff_id', { staff_id : '@id' });
 
 	return {
-		getArtResource : function()
-		{
-			return artResource;
-		}
+		getEndPoint         : function() { return endPoint;       },
+		getArtResource      : function() { return artResource;    },
+		getArtistsResource  : function() { return artistsResource;},
+		getEventsResource   : function() { return eventsResource; },
+		getVenuesResource   : function() { return venuesResource; },
+		getRegisterResource : function() {return registerResource;},
+		getProfileResource  : function() {return profileResource; },
+		getStaffResource    : function() {return staffResource;   },
 	}
 }]);

f22582a43d4b80fa0c09af808c13048846504863
Paden <padenportillo@gmail.com>
2014-02-16 14:06:21 -0600

    chosen library
    Signed-off-by: Paden <padenportillo@gmail.com>

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var depModules = ['ngRoute'    , 'monospaced.qrcode', 'ngAnimate' , 'mgcrea.ngStrap', 'mgcrea.ngStrap.helpers.parseOptions',
-				  'google-maps', 'webStorageModule' , 'ngResource', 'ngSanitize'];
+				  'google-maps', 'webStorageModule' , 'ngResource', 'ngSanitize'    ,  'localytics.directives'];
 
 angular.module('eastApp', depModules);
 angular.module('eastApp').config([ '$routeProvider', '$locationProvider', function($routeProvider, $locationProvider)
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,25 +4,29 @@ angular.module('eastApp').controller('MainCtrl', ['$scope', 'Artflask', function
 {
 	var artistsResource =  Artflask.getArtistsResource();
 
+	scope.select2Options = {
+		allowClear: false
+	}
+
 	scope.mediums = [
-		{ name : 'Painting'      , img: '<img src="images/painting.svg" style="max-width:20px;height:20px">Painting' },
-		{ name : 'Wood'          , img: '<img src="images/wood.svg" style="max-width:20px;height:20px">Wood' },
-		{ name : 'Jewelry'       , img: '<img src="images/jewelry.svg" style="max-width:20px;height:20px">Jewelry' },
-		{ name : 'Photography'   , img: '<img src="images/photography.svg" style="max-width:20px;height:20px">Photography' },
-		{ name : 'Stone'         , img: '<img src="images/stone.svg" style="max-width:20px;height:20px">Stone' },
-		{ name : 'Digital'       , img: '<img src="images/digital.svg" style="max-width:20px;height:20px">Digital' },
-		{ name : 'Sculpture'     , img: '<img src="images/sculpture.svg" style="max-width:20px;height:20px">Sculpture' },
-		{ name : 'Metal'         , img: '<img src="images/metal.svg" style="max-width:20px;height:20px">Metal' },
-		{ name : 'Ceramics'      , img: '<img src="images/ceramics.svg" style="max-width:20px;height:20px">Ceramics' },
-		{ name : 'Printed Matter', img: '<img src="images/printed-matter.svg" style="max-width:20px;height:20px">Printed Matter' },
-		{ name : 'Drawing'       , img: '<img src="images/drawing.svg" style="max-width:20px;height:20px">Drawing' },
-		{ name : 'Mixed Media'   , img: '<img src="images/mixed-media.svg" style="max-width:20px;height:20px">Mixed Media' },
-		{ name : 'Performance'   , img: '<img src="images/performance.svg" style="max-width:20px;height:20px">Performance' },
-		{ name : 'Furniture'     , img: '<img src="images/furniture.svg" style="max-width:20px;height:20px">Furniture' },
-		{ name : 'Fiber'         , img: '<img src="images/fiber.svg" style="max-width:20px;height:20px">Fiber' },
-		{ name : 'Installation'  , img: '<img src="images/installation.svg" style="max-width:20px;height:20px">Installation' },
-		{ name : 'Design'        , img: '<img src="images/design.svg" style="max-width:20px;height:20px">Design' },
-		{ name : 'Film/Video'    , img: '<img src="images/film-video.svg" style="max-width:20px;height:20px">Film/Video' }
+		{ name : 'Painting'      , html: '<img src="images/painting.svg" style="max-width:20px;height:20px">Painting' },
+		{ name : 'Wood'          , html: '<img src="images/wood.svg" style="max-width:20px;height:20px">Wood' },
+		{ name : 'Jewelry'       , html: '<img src="images/jewelry.svg" style="max-width:20px;height:20px">Jewelry' },
+		{ name : 'Photography'   , html: '<img src="images/photography.svg" style="max-width:20px;height:20px">Photography' },
+		{ name : 'Stone'         , html: '<img src="images/stone.svg" style="max-width:20px;height:20px">Stone' },
+		{ name : 'Digital'       , html: '<img src="images/digital.svg" style="max-width:20px;height:20px">Digital' },
+		{ name : 'Sculpture'     , html: '<img src="images/sculpture.svg" style="max-width:20px;height:20px">Sculpture' },
+		{ name : 'Metal'         , html: '<img src="images/metal.svg" style="max-width:20px;height:20px">Metal' },
+		{ name : 'Ceramics'      , html: '<img src="images/ceramics.svg" style="max-width:20px;height:20px">Ceramics' },
+		{ name : 'Printed Matter', html: '<img src="images/printed-matter.svg" style="max-width:20px;height:20px">Printed Matter' },
+		{ name : 'Drawing'       , html: '<img src="images/drawing.svg" style="max-width:20px;height:20px">Drawing' },
+		{ name : 'Mixed Media'   , html: '<img src="images/mixed-media.svg" style="max-width:20px;height:20px">Mixed Media' },
+		{ name : 'Performance'   , html: '<img src="images/performance.svg" style="max-width:20px;height:20px">Performance' },
+		{ name : 'Furniture'     , html: '<img src="images/furniture.svg" style="max-width:20px;height:20px">Furniture' },
+		{ name : 'Fiber'         , html: '<img src="images/fiber.svg" style="max-width:20px;height:20px">Fiber' },
+		{ name : 'Installation'  , html: '<img src="images/installation.svg" style="max-width:20px;height:20px">Installation' },
+		{ name : 'Design'        , html: '<img src="images/design.svg" style="max-width:20px;height:20px">Design' },
+		{ name : 'Film/Video'    , html: '<img src="images/film-video.svg" style="max-width:20px;height:20px">Film/Video' }
 	];
 }]);
 

f9be25a085cd619b4854f83e2981b6abe2379f42
Paden <padenportillo@gmail.com>
2014-03-02 09:37:01 -0600

    scrollto sponsors
    Signed-off-by: Paden <padenportillo@gmail.com>

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var depModules = ['ngRoute'    , 'monospaced.qrcode', 'ngAnimate' , 'mgcrea.ngStrap', 'mgcrea.ngStrap.helpers.parseOptions',
-				'google-maps', 'webStorageModule' , 'ngResource', 'ngSanitize'    ];
+				'google-maps', 'webStorageModule' , 'ngResource', 'ngSanitize', 'scrollto'    ];
 
 angular.module('eastApp', depModules);
 angular.module('eastApp').config( function( $routeProvider, $locationProvider )

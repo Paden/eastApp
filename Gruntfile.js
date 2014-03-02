14b8531f43eb84be6e0e27d0c0c5df7916bec7f1
Paden <padenportillo@gmail.com>
2014-03-01 18:45:19 -0600

    copy straight images
    Signed-off-by: Paden <padenportillo@gmail.com>

--- Gruntfile.js
+++ Gruntfile.js
@@ -255,7 +255,7 @@ module.exports = function (grunt) {
             '.htaccess',
             '*.html',
             'views/{,*/}*.html',
-            'images/{,*/}*.{webp}',
+            'images/{,*/}*.*',
             'fonts/*'
           ]
         }, {
